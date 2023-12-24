65edbe3825a918265a884fad4b8678f8850a7854
sleepman <luppovfrol@gmail.com>
2023-12-24 11:48:02 +0100

    dz 12

---
+++ 16-another-tasks/app.js
@@ -0,0 +1,29 @@
+function todo(){
+    const toDoList = {
+        task:[
+            {
+                title: "Помыть",
+                id:1,
+                priority:3,
+                description:"ку ку"
+            }
+        ],
+        addTask(title, priority) {
+            const id = this.task.map(id => id.id).sort((a, b) => b - a)[0] + 1
+            return this.task.push({title, id, priority}) 
+        },
+        removeTask(id){
+        const index = this.task.findIndex(i => i.id === id)
+        return this.task.splice(index, 1)
+        },
+        refreshTitle(title, id) {
+        const index = this.task.findIndex(i => i.id === id)
+        return this.task[index].title = title        
+        },
+        sortPriority(){
+        return this.task.sort((a,b) => b.priority - a.priority)
+        }
+    }
+
+    return toDoList
+}

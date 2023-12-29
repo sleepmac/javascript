94115c919c9570aea56e551d736b2384dc195f7b
sleepmac <luppovfrol@gmail.com>
2023-12-29 06:35:08 +0100

    up9

--- habbit/scripts/app.js
+++ habbit/scripts/app.js
@@ -7,11 +7,14 @@ const HABBIT_KEY = 'HABBIT_KEY';
 const page = {
 	menu: document.querySelector('.menu__list'),
 	header: {
-		h1:document.querySelector(".h1"),
-		progressPercent: document.querySelector(".progress__percent"),
-		progressCoverBar: document.querySelector(".progress__cover-bar")
-
-	}	
+		h1: document.querySelector('.h1'),
+		progressPercent: document.querySelector('.progress__percent'),
+		progressCoverBar: document.querySelector('.progress__cover-bar'),
+	},
+	content: {
+		daysContainer: document.getElementById('days'),
+		nextDay: document.querySelector('.habbit__day')
+	}
 }
 
 /* utils */
@@ -29,9 +32,6 @@ function saveData() {
 
 /* render */
 function rerenderMenu(activeHabbit) {
-	if (!activeHabbit) {
-		return;
-	}
 	for (const habbit of habbits) {
 		const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
 		if (!existed) {
@@ -54,23 +54,38 @@ function rerenderMenu(activeHabbit) {
 	}
 }
 
-function rerenderHead(activeHabbit){
-	if (!activeHabbit) {
-		return;
-	}
-	page.header.h1.innerText = activeHabbit.name
+function rerenderHead(activeHabbit) {
+	page.header.h1.innerText = activeHabbit.name;
 	const progress = activeHabbit.days.length / activeHabbit.target > 1
-	? 100
-	: activeHabbit.days.length / activeHabbit.target * 100
-	page.header.progressPercent.innerText = progress.toFixed(0) + "%"
-	page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
+		? 100
+		: activeHabbit.days.length / activeHabbit.target * 100;
+	page.header.progressPercent.innerText = progress.toFixed(0) + '%';
+	page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`);
+}
 
+function rerenderContent(activeHabbit) {
+	page.content.daysContainer.innerHTML = '';
+	for (const index in activeHabbit.days) {
+		const element = document.createElement('div');
+		element.classList.add('habbit');
+		element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
+              <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+              <button class="habbit__delete">
+                <img src="./images/delete.svg" alt="Удалить день ${index + 1}" />
+              </button>`;
+		page.content.daysContainer.appendChild(element);
+	}
+	page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`;
 }
 
 function rerender(activeHabbitId) {
 	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+	if (!activeHabbit) {
+		return;
+	}
 	rerenderMenu(activeHabbit);
-	rerenderHead(activeHabbit)
+	rerenderHead(activeHabbit);
+	rerenderContent(activeHabbit);
 }
 
 /* init */

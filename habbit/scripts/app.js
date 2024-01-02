2ac8ee02b711c5174f5d97f86aa8767d19d6df8f
sleepmac <luppovfrol@gmail.com>
2024-01-02 18:20:50 +0100

    up10

--- habbit/scripts/app.js
+++ habbit/scripts/app.js
@@ -2,6 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalActiveHabbitId
 
 /* page */
 const page = {
@@ -70,7 +71,7 @@ function rerenderContent(activeHabbit) {
 		element.classList.add('habbit');
 		element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
               <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
-              <button class="habbit__delete">
+              <button class="habbit__delete" onclick="deleteDay(${index})">
                 <img src="./images/delete.svg" alt="Удалить день ${index + 1}" />
               </button>`;
 		page.content.daysContainer.appendChild(element);
@@ -79,6 +80,7 @@ function rerenderContent(activeHabbit) {
 }
 
 function rerender(activeHabbitId) {
+	globalActiveHabbitId = activeHabbitId
 	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
 	if (!activeHabbit) {
 		return;
@@ -86,6 +88,45 @@ function rerender(activeHabbitId) {
 	rerenderMenu(activeHabbit);
 	rerenderHead(activeHabbit);
 	rerenderContent(activeHabbit);
+}
+
+/* work with days*/
+function addDays(event){
+	const form = event.target
+	event.preventDefault()
+	const data = new FormData(form)
+	const comment = data.get("comment")
+	form['comment'].classList.remove('error')
+	if(!comment){
+		form['comment'].classList.add('error')
+	}
+	habbits = habbits.map(habbit => {
+		if(habbit.id === globalActiveHabbitId){
+			return {
+				...habbit,
+				days: habbit.days.concat([{ comment }])
+			}
+		}
+		return habbit
+	})
+	form['comment'].value = ''
+	rerender(globalActiveHabbitId)
+	saveData()
+}
+
+function deleteDay(index){
+	habbits = habbits.map(habbit => {
+		if (habbit.id === globalActiveHabbitId){
+			habbit.days.splice(index, 1)
+			return {
+				...habbit,
+				days: habbit.days
+			}
+		}
+		return habbit
+	})
+	rerender(globalActiveHabbitId)
+	saveData()
 }
 
 /* init */

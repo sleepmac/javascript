99a63b86683b0a10a454248823591397cfcb6a38
sleepmac <luppovfrol@gmail.com>
2024-01-04 12:17:33 +0100

    up15

--- habbit/scripts/app.js
+++ habbit/scripts/app.js
@@ -127,6 +127,7 @@ function rerender(activeHabbitId) {
 	if (!activeHabbit) {
 		return;
 	}
+	document.location.replace(document.location.pathname + '#' + activeHabbitId);
 	rerenderMenu(activeHabbit);
 	rerenderHead(activeHabbit);
 	rerenderContent(activeHabbit);
@@ -200,5 +201,11 @@ function addHabbit(event) {
 /* init */
 (() => {
 	loadData();
-	rerender(habbits[0].id)
+	const hashId = Number(document.location.hash.replace('#', ''));
+	const urlHabbit = habbits.find(habbit => habbit.id == hashId);
+	if (urlHabbit) {
+		rerender(urlHabbit.id);
+	} else {
+		rerender(habbits[0].id);
+	}
 })();
